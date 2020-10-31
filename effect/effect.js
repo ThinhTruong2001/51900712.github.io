401a8ec6a4df3691add3ef07604b807024dc391a
Thinh Truong <58397636+ThinhTruong2001@users.noreply.github.com>
2020-11-01 03:51:06 +0700

    Add files via upload

--- effect/effect.js
+++ effect/effect.js
@@ -1,7 +1,9 @@
 $('document').ready(function() {
     $('.container').fadeIn('slow').delay(500);
     $('#begin').click(function() {
-        $("#song").get(0).play();
+        var song = document.getElementById("song");
+        song.play();
+        song.volume = 0.5;
         $('body').addClass('background_add');
         $(this).fadeOut('slow');
         $('.Banner').fadeIn('slow');
@@ -28,8 +30,7 @@ $('document').ready(function() {
         }, 5000);
 
         setTimeout(function() {
-            var x = document.getElementById("song");
-            x.pause();
+            song.pause();
             $('.Banner').addClass('bannerAnimation');
             $('.balloons').fadeIn('slow').delay(500);
             randomPosition('balloon1');
@@ -37,49 +38,23 @@ $('document').ready(function() {
             randomPosition('balloon3');
             randomPosition('balloon4');
             randomPosition('balloon5');
-            $('#song2').get(0).play();
+            song = document.getElementById("song2");
+            song.play();
+            song.volume = 0.5;
             $('.balloons').addClass('balloons-rotate');
             $('.flip-card').fadeIn('slow').delay(1000);
-            $('#card ').fadeIn('slow').delay(1500);
+            $('.card ').fadeIn('slow').delay(1500);
         }, 34000);
 
         function randomPosition(id) {
             var width = $(window).width();
             var height = $(window).height();
-            var Hor = (8000 * Math.random()) % width;
-            var Ver = (10000 * Math.random()) % height;
+            var Hor = (8000 * Math.random()) % (width - 100);
+            var Ver = (10000 * Math.random()) % (height * 50 / 100);
             var balloon = document.getElementById(id);
-            $(balloon).animate({ left: Hor, bottom: Ver }, 8000, function() {
+            $(balloon).animate({ left: Hor, top: Ver }, 8000, function() {
                 randomPosition(id);
             });
         };
     });
-
-    (function() {
-        function $(id) {
-            return document.getElementById(id);
-        }
-        var card = $('card'),
-            open = $('open'),
-            close = $('close'),
-            timer = null;
-        console.log(card);
-        open.addEventListener('click', function() {
-            card.setAttribute('class', 'open-half');
-            if (timer) clearTimeout(timer);
-            timer = setTimeout(function() {
-                card.setAttribute('class', 'open-fully');
-                timer = null;
-            }, 1000);
-        });
-
-        close.addEventListener('click', function() {
-            card.setAttribute('class', 'close-half');
-            if (timer) clearTimerout(timer);
-            timer = setTimeout(function() {
-                card.setAttribute('class', '');
-                timer = null;
-            }, 1000);
-        });
-    }());
 });
