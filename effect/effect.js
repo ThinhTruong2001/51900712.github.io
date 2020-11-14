2b1c9eaec0ce71fc692afd376fa5038ad12d4ca5
Thinh Truong <58397636+ThinhTruong2001@users.noreply.github.com>
2020-11-15 02:30:14 +0700

    Add files via upload

--- effect/effect.js
+++ effect/effect.js
@@ -1,58 +1,107 @@
 $('document').ready(function() {
     $('.container').fadeIn('slow').delay(500);
+    const pages = Array.from(document.querySelectorAll(".card-container .card"));
+    let leftStack = [];
+    let rightStack = Array.from(pages).reverse();
+    updatePagesDepth(rightStack);
+    for (const page of pages) {
+        page.addEventListener("click", function(e) {
+            if (e.currentTarget.classList.contains("canFlip")) {
+                const page = leftStack.pop();
+                rightStack.push(page);
+                page.classList.remove("canFlip");
+                updatePagesDepth(rightStack);
+            } else {
+                const page = rightStack.pop();
+                leftStack.push(page);
+                page.classList.add("canFlip");
+                updatePagesDepth(leftStack);
+            };
+        });
+    };
+
+    function updatePagesDepth(stack) {
+        for (const [i, page] of stack.entries()) {
+            if (stack == leftStack) {
+                page.style.transform = `rotateY(-180deg) translateZ(${-i}px)`;
+            } else {
+                page.style.transform = `rotateY(0) translateZ(${i}px)`;
+            }
+        }
+    };
+    $('.ballloons').mouseover(function() {
+        var balloon = document.getElementById(this);
+        randomPosition(balloon);
+    });
+    var currentDeg = 0;
+    $('.carousel').click(function() {
+        currentDeg = currentDeg - 60;
+        $('.carousel').css({
+            "transform": "rotateY(" + currentDeg + "deg)"
+        })
+    });
+    $('.closeBtn').click(function() {
+        $('.container-carousel').fadeOut('slow').promise().done(function() {
+            $('.card-container').fadeIn('slow').delay(1500);
+            $('.closeBtn').slideUp('slow').delay(2000);
+        });
+    });
+    $('.image').click(function() {
+        $('.card-container').fadeOut('slow').promise().done(function() {
+            $('.container-carousel').fadeIn('slow').delay(1500);
+            $('.closeBtn').slideDown('slow').delay(2000);
+        })
+    });
     $('#begin').click(function() {
         var song = document.getElementById("song");
         song.play();
         song.volume = 0.5;
-        $('body').addClass('background_add');
         $(this).fadeOut('slow');
         $('.Banner').fadeIn('slow');
+        $('.border-left').fadeIn('slow');
+        $('.border-right').fadeIn('slow');
         setTimeout(function() {
             $('.message').fadeIn('slow');
             var i;
 
             function msgLoop(i) {
-                $(".message p:nth-child(" + i + ")").fadeOut('slow').delay(800).promise().done(function() {
+                $(".message p:nth-child(" + i + ")").fadeOut('slow').delay(1000).promise().done(function() {
                     i = i + 1;
-                    $(".message p:nth-child(" + i + ")").fadeIn('slow').delay(1200);
-                    if (i == 9) {
-                        $(".message p:nth-child(" + i + ")").fadeOut('slow').delay(800);
+                    if (i == 2) {
+                        $(".message p:nth-child(" + i + ")").fadeIn('slow').delay(1500);
+                    } else {
+                        $(".message p:nth-child(" + i + ")").fadeIn('slow').delay(10000);
+                    }
+                    if (i == 3) {
+                        $(".message p:nth-child(" + i + ")").fadeOut('slow').delay(1000);
                         return;
-
                     } else {
                         msgLoop(i);
                     }
-
                 });
-
-            }
+            };
             msgLoop(0);
-        }, 5000);
+        }, 3000);
 
         setTimeout(function() {
-            song.pause();
             $('.Banner').addClass('bannerAnimation');
-            $('.balloons').fadeIn('slow').delay(1500);
+            $('.balloons').fadeIn('slow').delay(500);
             randomPosition('balloon1');
             randomPosition('balloon2');
             randomPosition('balloon3');
             randomPosition('balloon4');
             randomPosition('balloon5');
-            song = document.getElementById("song2");
-            song.play();
-            song.volume = 0.5;
             $('.balloons').addClass('balloons-rotate');
-            $('.flip-card').fadeIn('slow').delay(1000);
-            $('.card ').fadeIn('slow').delay(1500);
-        }, 34000);
+            $('.card-container').fadeIn('slow').delay(1500);
+        }, 30000);
 
         function randomPosition(id) {
             var width = $(window).width();
             var height = $(window).height();
-            var Hor = (8000 * Math.random()) % (width - 100);
+            var Hor = (8000 * Math.random()) % (width * 80 / 100);
             var Ver = (10000 * Math.random()) % (height * 50 / 100);
             var balloon = document.getElementById(id);
-            $(balloon).animate({ left: Hor, top: Ver }, 8000, function() {
+            $(balloon).animate({ left: 10 + Hor, top: 10 + Ver }, 8000, function() {
                 randomPosition(id);
             });
         };
